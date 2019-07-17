8268f99b95c91d8f95c9009b303aca9c50e57446
Rafael Dias <rafael.dias@outlook.com>
2019-07-17 17:51:27 -0300

    Inclusão de campos de where

--- controller/api_controller.js
+++ controller/api_controller.js
@@ -29,11 +29,17 @@ exports.getTarifas = (req, res, next) => {
             // create Request object
             var request = new sql.Request();
             // query to the database
-            const {linha,} = req.query;
+            const {linha,municipio_origem,municipio_destino} = req.query;
             let where = "";
        
             if (linha) {
                where = "and lin.codlin = '"+ linha + "'";
+            }
+            if (municipio_origem) {
+                where = "and mun_ori.codmun = '"+ municipio_origem + "'";
+            }
+            if (municipio_destino) {
+                where = "and mun_des.codmun = '"+ municipio_destino + "'";
             }
             var query = "select 'FR' tipo_operacao,mun_ori.codmun codigo_municipio_origem,mun_ori.descri municipio_origem,mun_ori.estado uf_municipio_origem,mun_des.codmun codigo_municipio_destino,mun_des.descri municipio_destino,mun_des.estado uf_municipio_destino,lin.codlin,lin.porto_origem,lin.descri_porto_origem,lin.estado_porto_origem,lin.porto_destino,lin.descri_porto_destino,lin.estado_porto_destino,tar.vlrpor_kg,tar.vlrpor_m3,tar.vlrtaxa_cte,tar.pesomin_kg,tar.m3min,tar.perc_advalorem_receber,tar.perc_advalorem_pagar,tar.perc_gris,tar.vlrcusto_referencia,tar.vlrfrete_peso,tar.vlrpedagio,tar.vlradicional,tar.faixapeso_inicial,tar.faixapeso_final from db_webcab.dbo.cotacao_tabela_linha_servicos lin inner join db_webcab.dbo.cotacao_tabela_tarifas tar on tar.id_servico = lin.indice inner join db_webcab.dbo.rodlin rl on rl.codlin = lin.codlin inner join db_webcab.dbo.rodmun mun_ori on mun_ori.coditn = rl.ponini inner join db_webcab.dbo.rodmun mun_des on mun_des.coditn = rl.ponfim where lin.situacao = 'A' and lin.id_tabela = 1 and tar.situacao = 'A' "+ where +" order by lin.codlin";
             //console.log(query);
